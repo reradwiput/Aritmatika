bf431c5cd7ec60c6744d82c3818d268c3010d4be
reradwiput <144641062+reradwiput@users.noreply.github.com>
2023-09-11 09:23:34 +0700

    Create Aritmatika.cpp

---
+++ Aritmatika.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int a, b;
+cout <<"Pemograman operasi penjumlahan \n";
+cout<<"==============================\n";
+cout<<"Masukan angka pertama: ";
+cin>>a;
+cout<<" Masukan angka kedua: ";
+cin>>b;
+cout<<" Hasilnya penjumlahan: "<<a+b<<endl;
+cout<<" Hasilnya pengurangan: "<<a-b<<endl;
+cout<<" Hasilnya perkalian: "<<a*b<<endl;
+cout<<" Hasilnya pembagian: "<<a/b<<endl;
+cout<<" Hasilnya modulus: "<<a%b<<endl;
+return 0;
+}
